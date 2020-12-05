5d72cd9c41d3780169d3b052495befcfdc8e470b
eric <>
2020-12-04 22:22:45 -0500

    user model

---
+++ src/main/java/com/example/cs4550criticalprojectionsserverjava/models/User.java
@@ -0,0 +1,62 @@
+package main.java.com.example.cs4550criticalprojectionsserverjava.models;
+
+// @Entity
+// @Table(name = 'users')
+public class User{
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    public User(Integer id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
